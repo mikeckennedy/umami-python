d988f075ef7588a90fe3cfd6c36e5cbcb389c9b4
Michael Kennedy <mikeckennedy@gmail.com>
2025-05-30 19:15:47 -0700

    **Adding New Functions:** - `heartbeat_async`: An asynchronous version of the existing `heartbeat` function. - `heartbeat`: A synchronous version of the existing `heartbeat` function.

--- umami/umami/__init__.py
+++ umami/umami/__init__.py
@@ -1,7 +1,7 @@
 from umami import impl
 from . import errors  # noqa: F401, E402
 from . import models  # noqa: F401, E402
-from .impl import login_async, login, is_logged_in, heartbeat  # noqa: F401, E402
+from .impl import login_async, login, is_logged_in, heartbeat_async, heartbeat  # noqa: F401, E402
 from .impl import new_event_async, new_event  # noqa: F401, E402
 from .impl import new_page_view, new_page_view_async  # noqa: F401, E402
 from .impl import set_url_base, set_website_id, set_hostname  # noqa: F401, E402
@@ -12,14 +12,17 @@ __author__ = 'Michael Kennedy <michael@talkpython.fm>'
 __version__ = impl.__version__
 user_agent = impl.user_agent
 
+# fmt: off
+# ruff: noqa
 __all__ = [
-    models,
-    errors,
-    set_url_base, set_website_id, set_hostname,
-    login_async, login, is_logged_in,
-    websites_async, websites,
-    new_event_async, new_event,
-    new_page_view, new_page_view_async,
-    verify_token_async, verify_token,
-    heartbeat,
+    'models',
+    'errors',
+    'set_url_base', 'set_website_id', 'set_hostname',
+    'login_async', 'login', 'is_logged_in',
+    'websites_async', 'websites',
+    'new_event_async', 'new_event',
+    'new_page_view', 'new_page_view_async',
+    'verify_token_async', 'verify_token',
+    'heartbeat_async', 'heartbeat',
 ]
+# fmt: on
